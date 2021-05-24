7a4ef0176ed5c8ee8545ad793bd9911e0d1e969e
Valeriia <valeriastanis@gmail.com>
2021-05-24 15:55:43 +0200

    feat:implement scroll to top

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
   /// toogle burger-menu
   const BURGER_MENU = document.querySelector('.burger-menu'),
-        NAV_MENU = document.querySelector('.header__nav-menu');
+    NAV_MENU = document.querySelector('.header__nav-menu');
 
 
   function toggleBurgerMenu() {
@@ -19,4 +19,30 @@ document.addEventListener('DOMContentLoaded', (event) => {
     });
   };
   toggleBurgerMenu();
+
+  /// scrollToTop
+  const SCROLL_BTN = document.querySelector('.scroll-top');
+
+  function scrollToTop() {
+    document.documentElement.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  }
+
+  function scrollBtnHandler() {
+    let scrollTotal = document.documentElement.scrollHeight - document.documentElement.clientHeight
+    if ((document.documentElement.scrollTop / scrollTotal) > 0.50) {
+      SCROLL_BTN.classList.add('show-btn')
+    } else {
+      SCROLL_BTN.classList.remove('show-btn')
+    }
+  }
+
+  SCROLL_BTN.addEventListener('click', scrollToTop);
+  document.addEventListener('scroll', scrollBtnHandler);
+
+
+
+
 });
